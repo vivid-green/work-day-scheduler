bcd827f675fb413d7fb3d20fbd0c7514b4fc5a2b
Vivid Green <zacharycreek@gmail.com>
2020-08-22 18:17:55 -0600

    added comments to code blocks.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
-
-// Setting variables for day start and day end for calender. Also setting duration and hoursDif for time block element creation.
-// let now = moment().format("h a");
-let now = moment("2 pm", "h a");
+let now = moment(moment(), "h a");
+// now variable below is used for testing by adjusting the "current time" to whatever you want.
+// let now = moment("1 pm", "h a");
 let dayStart = moment("9 am", "h a");
 let dayEnd = moment("5 pm", "h a");
 let addTime = dayStart.clone();
@@ -14,14 +13,14 @@ let containerElement = $(".container");
 let events = localStorage.getItem('events');
 events = events ? JSON.parse(events) : {};
 let localStorageValue = events[localStorageId];
-console.log(events);
+// console.log(events);
 
 addTimeBlocks();
 
 function addTimeBlocks () {
-
+    //loop to start creating and appending html elements for time blocks.
     while (hoursDif > -1) {
-        
+        //variables to store newly creating html elements.
         let rowElement = $("<div class='row'>");
         let colElement = $("<div class='col-md-12'>");
         let inputGrpElement = $("<div class='input-group'>");
@@ -30,7 +29,7 @@ function addTimeBlocks () {
         let txtAreaFormControlElement = $("<textarea class='form-control'>");
         let appendInputGrpElement = $("<div class='input-group-append'>");
         let saveBtnElement = $("<button class='btn btn-primary btn-lg saveBtn' type='button'></button>");
-
+        //Appending elements to DOM
         containerElement.append(rowElement);
         rowElement.append(colElement);
         colElement.append(inputGrpElement);
@@ -39,7 +38,7 @@ function addTimeBlocks () {
         prependInputGrpElement.append(spanInputGrpTxtElement);
         inputGrpElement.append(txtAreaFormControlElement);
         txtAreaFormControlElement.attr("id", "input-" + localStorageId);
-        console.log(events[localStorageId]);
+        // console.log(events[localStorageId]);
         localStorageValue ? $("#input-" + localStorageId).val(localStorageValue) : $("#input-" + localStorageId).val("");
         txtAreaFormControlElement.css("color", "black");
         inputGrpElement.append(appendInputGrpElement);
@@ -49,7 +48,7 @@ function addTimeBlocks () {
         let past = now.isAfter(addTime, "hour");
         let present = now.isSame(addTime, "hour");
         let future = now.isBefore(addTime, "hour");
-
+        //if block to add respective class to textarea elements based on moment.js variables above.
         if(past) {
             txtAreaFormControlElement.addClass("past");
         } else if (present) {
@@ -57,16 +56,17 @@ function addTimeBlocks () {
         } else {
             txtAreaFormControlElement.addClass("future");
         }
-
+        //incrementing and updating variables for time. 
         addTime = addTime.add(1, "hour");
         hour = addTime.format("h a");
+        //saving events to local storage.
         localStorageId = hour.replace(/ /g,"-");
         localStorageValue = events[localStorageId];
         duration = moment.duration(dayEnd.diff(addTime));
         hoursDif = parseInt(duration.asHours());
     }
 }
-
+//Saving input from relative button element clicked.
 function saveEvent() {
     let inputId = "#input-" + this.id;
     let inputValue = $(inputId).val();
